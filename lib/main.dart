aec42fbb476f64e4520c726f87d75de9db3f75a9
Henrique Gouveia <h.gouveia@icloud.com>
2020-04-13 14:35:43 -0300

    init: ripple for flutter

---
+++ lib/main.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'dart:math' as math show sin, pi, sqrt;
+
+import 'package:flutter/animation.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+
+class Ripples extends StatefulWidget {
+  const Ripples({
+    Key key,
+    this.size = 80.0,
+    this.color = Colors.blue,
+    this.onPressed,
+    @required this.child,
+  }) : super(key: key);
+
+  final double size;
+  final Color color;
+  final Widget child;
+  final VoidCallback onPressed;
+
+  @override
+  _RipplesState createState() => _RipplesState();
+}
+
+class _CirclePainter extends CustomPainter {
+  _CirclePainter(
+      this._animation, {
+        @required this.color,
+      }) : super(repaint: _animation);
+
+  final Color color;
+  final Animation<double> _animation;
+
+  void circle(Canvas canvas, Rect rect, double value) {
+    final double opacity = (1.0 - (value / 4.0)).clamp(0.0, 1.0);
+    final Color _color = color.withOpacity(opacity);
+
+    final double size = rect.width / 2;
+    final double area = size * size;
+    final double radius = math.sqrt(area * value / 4);
+
+    final Paint paint = Paint()..color = _color;
+    canvas.drawCircle(rect.center, radius, paint);
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Rect rect = Rect.fromLTRB(0.0, 0.0, size.width, size.height);
+
+    for (int wave = 3; wave >= 0; wave--) {
+      circle(canvas, rect, wave + _animation.value);
+    }
+  }
+
+  @override
+  bool shouldRepaint(_CirclePainter oldDelegate) => true;
+}
+
+class _RipplesState extends State<Ripples> with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget _button() {
+    return Center(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(widget.size),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: <Color>[
+                widget.color,
+                Color.lerp(widget.color, Colors.black, .05)
+              ],
+            ),
+          ),
+          child: ScaleTransition(
+            scale: Tween(begin: 0.95, end: 1.0).animate(
+              CurvedAnimation(
+                parent: _controller,
+                curve: const _PulsateCurve(),
+              ),
+            ),
+            child: widget.child,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _CirclePainter(
+        _controller,
+        color: widget.color,
+      ),
+      child: SizedBox(
+        width: widget.size * 2.125,
+        height: widget.size * 2.125,
+        child: _button(),
+      ),
+    );
+  }
+}
+
+class _PulsateCurve extends Curve {
+  const _PulsateCurve();
+
+  @override
+  double transform(double t) {
+    if (t == 0 || t == 1) {
+      return 0.01;
+    }
+    return math.sin(t * math.pi);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+          child: Ripples()
+      ),
+    );
+  }
+}
